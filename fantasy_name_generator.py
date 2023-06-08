6added9a21afc6b20cdb3077f9b27c6bd2ae2c7e
Guillermo Santiago <g.santiago3rd@gmail.com>
2023-06-08 16:31:20 -0600

    refactored and more names

--- fantasy_name_generator.py
+++ fantasy_name_generator.py
@@ -1,34 +1,42 @@
 import random
 
 
-print("Welcome to the fantasy name generator\n")
+def generate_name():
+    first_names = ("Rossshu","Al","Johnrek","Merlphy","Elmlee","Mephistophflores",
+                   "Vikgraham","Mesmoke","Fornwest","Hacrusher","Badcrawford",
+                   "Petersoniniar","Gwydellis","Harmmez", "Hatianaka"
+                   "Wolda","Fraffolk","Berthelm","Cyne","Cuthbehrt","Wolda","Reda",
+                   "Aethrin","Amus","Arsin","Sumuru","Nazuuma","Biumadea")
 
-first_names = ("Al","Johnrek","Merlphy","Elmlee","Mephistophflores",
-               "Vikgraham","Mesmoke","Fornwest","Hacrusher","Badcrawford",
-               "Petersoniniar","Gwydellis")
+    last_names = ("Aumcia","Aumrvey","Firephens","Fangryan","Williamsang",
+                  "Kellydrinker", "Drottmartin", "Hernananic", "Satanlopez",
+                  "Ronic", "Dianic", "Gophire", "De Hollandcourt", "Fireckson"
+                  "Tahyrst","Cheawood", "Demoor", "Adham", "Bratun", "Caford", "Famor", 
+                  "Wahyrst", "Moford","Adbrycg","Choogate","Brewood","Wawold")
 
-last_names = ("Aumcia","Aumrvey","Firephens","Fangryan","Williamsang",
-              "Kellydrinker", "Drottmartin", "Hernananic", "Satanlopez",
-                "Ronic", "Dianic", "Gophire")
-
-chosen_first_name = ""
-chosen_last_name = ""
-
-
-while True:
     chosen_first_name = random.choice(first_names)
-
     chosen_last_name = random.choice(last_names)
 
-    print("\n")
-    print("{} {}".format(chosen_first_name, chosen_last_name))
-    print("\n")
+    return "{} {}".format(chosen_first_name, chosen_last_name)
 
-    try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
+def main():
+    print("Welcome to the fantasy name generator\n")
 
-    if try_again.lower() == "n":
-        break
+    while True:
+        print("\n")
+        print(generate_name())
+        print("\n")
 
-input("\nPress Enter to exit.")
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
+
+        if try_again.lower() == "n":
+            break
+
+    input("\nPress Enter to exit.")
+
+
+if __name__ == "__main__":
+    main()
+
 
 
